c1aea5c33f83ddf6db23538495b525a2bef644e6
Harshad Bhandare <harshadbhandare25@gmail.com>
2023-06-03 14:23:24 +0530

    Update MarvellousPacker.java

--- MarvellousPacker.java
+++ MarvellousPacker.java
@@ -7,55 +7,77 @@ class MarvellousPacker
     public static void main(String a[])
     {
         Scanner sobj = new Scanner(System.in);
-        System.out.println("--------- Marvellous Packer Unpacker CUI Panel --------- ");
+
+        System.out.println("------------------Marvellous Packer Unpacker CUI Panel---------------");
 
         try
         {
-            System.out.println("Enter the Folder name : ");
-            String strr = sobj.nextLine();
-            
-            System.out.println("Enter the name of packed file : ");
-            System.out.println("Note : Packed file should be in the current directory.");
+            System.out.println("Enter folder name which contains a file which you want pack : ");
+            String FolderName = sobj.nextLine();
 
+            File fobj = new File(FolderName);
+
+            System.out.println("Enter the name of packed file that you want to create :  ");
+            System.out.println("Note :  Packed file gets stored automatically in current directory !!");
             String PackFile = sobj.nextLine();
+
             File fpackobj = new File(PackFile);
+            fpackobj.createNewFile();
 
-            FileInputStream fin = new FileInputStream(fpackobj);
-            int Ret = 0,iCount = 0;
-            byte Header[] = new byte[100];
+            FileOutputStream fout = new FileOutputStream(fpackobj);
 
-            if(fpackobj.exists())
+            if(fobj.exists())
             {
-                while((Ret = fin.read(Header,0,100)) > 0)
+                File allfiles[] = fobj.listFiles();
+
+                System.out.println("Number of files are : "+allfiles.length);
+
+                System.out.println("File names are : ");
+                int count= 0;
+
+                byte Buffer[] = new byte[1024];
+
+                int ret = 0;
+                String name;
+
+                for(int i = 0; i< allfiles.length; i++)
                 {
-                    String StrHeader = new String(Header);
+                    name = allfiles[i].getName(); 
 
-                    String Arr[] = StrHeader.split(" ");
+                     
+                     if(name.endsWith(".txt"))
+                     {
+                         
+                        System.out.println("Header  : "+name+" With Length "+name.length());
 
-                    File obj = new File(Arr[0]);
-                    obj.createNewFile();
+                        for(int j  = name.length();j<100;j++)
+                         {
+                          
+                            name = name + " ";
+                
+                         }  
 
-                    int FileSize = Integer.parseInt(Arr[1]);
+                        byte HeaderByte[]  = name.getBytes();        //String to byte array conversion
 
-                    byte DataArray[] = new byte[FileSize];
+                        fout.write(HeaderByte,0,HeaderByte.length); //Write Header in Packer
 
-                    fin.read(DataArray,0,FileSize);
+                        FileInputStream fiobj = new FileInputStream(allfiles[i]);
 
-                    FileOutputStream fout = new FileOutputStream(obj);
-                    fout.write(DataArray,0,FileSize);
-
-                    iCount++;
+                         while((ret = fiobj.read(Buffer)) != -1)
+                         {
+                            fout.write(Buffer,0,ret);
+                         }
+                         count++;
+                   }
                 }
+
                 System.out.println("-----------Summary---------");
-          
-                System.out.println("Number of file packed Successfully "+iCount);
-
-                System.out.println("Thank you for using Marvellous Packer Unpacker Application");
-
+                System.out.println("Number of files Scanned : "+allfiles.length);
+                System.out.println("Number of file packed Successfully "+count);
             }
             else
             {
-                System.out.println("There is no such file..");
+                System.out.println("There is no such folder..");
             }
 
         }// End of try
